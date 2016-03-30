8f7965c7e5709d795e5597506c2329c6d0599630
Christian R. Vozar <christian@rogueethic.com>
2016-03-30 14:15:44 -0500

    100% test coverage

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -5,22 +5,25 @@ package fizzbuzz
 import "testing"
 
 func TestFizz(t *testing.T) {
-	v := Determine(9)
-	if v != "Fizz" {
+	if v := Determine(9); v != "Fizz" {
 		t.Error("Exepected Fizz, got ", v)
 	}
 }
 
 func TestBuzz(t *testing.T) {
-	v := Determine(10)
-	if v != "Buzz" {
+	if v := Determine(10); v != "Buzz" {
 		t.Error("Exepected Buzz, got ", v)
 	}
 }
 
 func TestFizzBuzz(t *testing.T) {
-	v := Determine(30)
-	if v != "FizzBuzz" {
+	if v := Determine(30); v != "FizzBuzz" {
 		t.Error("Exepected FizzBuzz, got ", v)
+	}
+}
+
+func TestInvalid(t *testing.T) {
+	if v := Determine(4); v != "4" {
+		t.Error("Exepected 4, got ", v)
 	}
 }
